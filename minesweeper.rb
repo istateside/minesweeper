5f15f2d494c6122420a99a0a41d88924337261f3
Kevin Fleischman <kevin.r.fleischman@gmail.com>
2014-09-15 17:01:51 -0400

    final working version incl YAML

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 class Tile
 
   NEIGHBOR_POS = [
@@ -16,7 +17,7 @@ class Tile
 
   def initialize(board, x, y)
     @is_bomb = false
-    @neighbors = nil
+    @neighbors = []
     @revealed = false
     @is_flagged = false
     @neighbor_bomb_count = 0
@@ -49,9 +50,9 @@ class Tile
 
   def print_tile
     # return a string depending on the state
-    return 'B' if @is_bomb
     return 'F' if @is_flagged
     if @revealed
+      return 'B' if @is_bomb
       @neighbor_bomb_count == 0 ? '_' : @neighbor_bomb_count
     else
       '*'
@@ -63,13 +64,16 @@ class Tile
   end
 
   def reveal_tile
+
     @revealed = true
 
     if @is_bomb
-      reveal_all_bombs
       return
     elsif @neighbor_bomb_count == 0
-      @neighbors.each { |neighbor| neighbor.reveal_tile }
+      unrevealed_neighbors = @neighbors.reject do |neighbor|
+        neighbor.revealed
+      end
+      unrevealed_neighbors.each { |neighbor| neighbor.reveal_tile }
     end
     nil
   end
@@ -93,28 +97,21 @@ class Board
   end
 
   def place_bombs
-    all_tiles = []
-    @rows.each do |row|
-      row.each do |tile|
-        all_tiles << tile
-      end
-    end
-    all_tiles.sample(10).each do |tile|
-      tile.is_bomb = true
-    end
+    @rows.flatten.sample(10).each { |tile| tile.is_bomb = true }
   end
 
   def get_all_neighbor_bomb_counts
-    @rows.each do |row|
-      row.each do |tile|
-        tile.get_neighbor_bomb_count
-      end
-    end
+    @rows.flatten.each { |tile| tile.get_neighbor_bomb_count }
     nil
   end
 
   def blank_grid
     grid = Array.new(SIZE) { Array.new(SIZE) }
+
+    # grid = Array.new(SIZE) do |row_index|
+#       Array.new(SIZE) { |col_index| Tile.new }
+#     end
+
     grid.each_with_index do |row, row_index|
       col_index = 0
       while col_index < row.length
@@ -133,12 +130,18 @@ class Board
   def display
     @rows.each do |row|
       line = []
-      row.map do |tile|
-        line << tile.print_tile
-      end
+      row.each { |tile| line << tile.print_tile }
       puts line.join (' ')
     end
     nil
+  end
+
+  def flag_spot(pos)
+    if self[pos].nil? || self[pos].is_flagged
+      return false
+    else
+      self[pos].flag_tile
+    end
   end
 
   def reveal_spot(pos)
@@ -155,30 +158,33 @@ class Board
     true
   end
 
-  def flag_spot(pos)
-    if self[pos].nil? || self[pos].is_flagged
-      return false
-    else
-      self[pos].flag_tile
+  def reveal_all_bombs
+    @rows.each do |row|
+      row.each do |tile|
+        if tile.is_bomb
+          tile.revealed = true
+          tile.is_flagged = false
+        end
+      end
     end
   end
 
   def loss?
     @rows.each do |row|
-       row.any? { |tile| tile.is_bomb && tile.revealed }
+      #row.any? { |tile| tile.is_bomb && tile.revealed }
+      row.each do |tile|
+        if tile.is_bomb && tile.revealed
+          return true
+        end
+      end
     end
+    false
   end
 
   def won?
     @rows.all? do |row|
-      row.all? do |tile|
-        tile.is_bomb ? next : tile.revealed
-      end
+      row.all? { |tile| tile.is_bomb ? next : tile.revealed }
     end
-  end
-
-  def reveal_all_bombs
-    # build out
   end
 
 end
@@ -193,49 +199,82 @@ class Game
 
     # puts "Enter a command (f for flag, r for reveal) and a position (x,y)"
     puts "Select a position. x,y"
-    pos = gets.chomp.split(',')
+    input = gets.chomp
+
+    if input.downcase == 'save'
+      save_game
+      return
+    else
+      pos = input.split(',').map { |num| num.to_i }.reverse
+    end
+
+    # begin
+#       pos = Integer(num)
+#     rescue ArgumentError
+#       puts "Please put in valid move!!"
+#       retry
+#     end
+
+    if @board[pos].nil? || @board[pos].revealed
+      puts "Please enter valid coordinates."
+      return
+    end
 
     puts "Flag (f) or reveal (r)?"
     command = gets.chomp.downcase
 
-    until ['f','r'].include?(command)
-      if command == 'r'
-        until @board.reveal_spot(pos)
-          puts "Please enter valid coordinates."
-          pos = gets.chomp.split(',')
-        end
-      elsif command == 'f'
-        until @board.flag_spot(pos)
-          puts "Please enter valid coordinates."
-        end
-      else
-        puts "Please enter valid command."
-        command = gets.chomp.downcase
-      end
+    if !['f','r', 'flag', 'reveal'].include?(command)
+      puts "Invalid move specified."
+      return
+    end
+
+    if ['r', 'reveal'].include?(command)
+      @board.reveal_spot(pos)
+    else
+      @board.flag_spot(pos)
     end
   end
 
   def play
+    start_time = Time.now
     loop do
-      # until the game is won
-      # check if player lost - check if any bomb is revealed
       @board.display
 
       get_player_choice
 
-      # check if (a) all non-bombs revealed and (b) all bombs flagged
-      if @board.loss?
-        puts "YOU LOSE."
-        break
-      elsif @board.won?
-        puts "You win!"
-        break
-      end
-
+      break if @board.loss? || @board.won?
+    end
+    if @board.loss?
+      @board.display
+      puts "YOU LOSE."
+      puts "Time elapsed: #{Time.now - start_time}"
+    elsif @board.won?
+      @board.display
+      puts "You win!"
+      puts "Time elapsed: #{Time.now - start_time}"
     end
   end
-    #
-  # def over?
-  # end
 
+  def save_game
+    File.open("minesweeper_save.yaml", "w") do |file|
+      file.puts self.to_yaml
+    end
+  end
+
+  def self.load_game
+    loaded_game = YAML.load_file("minesweeper_save.yaml")
+  end
 end
+
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Welcome to MINESWEEPER"
+  puts "NEW game or LOAD existing game (N or L)?"
+  user_input = gets.chomp.downcase
+  if user_input == 'n'
+    Game.new.play
+  else
+    Game.load_game.play
+  end
+end
+
